b8ecd617f72414c652b4a620688bbda530e89acb
arrC <naveen@arrc.in>
2016-06-24 15:29:16 +0530

    created aircraft model, editor route

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,9 +1,42 @@
 'use strict';
 
-let role = ['Attack', 'Aerobatic', 'Trainer', 'Bomber', 'Fighter', 'Ground attack', 'Missile', 'Maritime patrol', 'Reconnaissance', 'Surveillance', 'Transport', 'Tanker', 'Utility' ];
-let industry = ['commercial', 'private', 'military'];
+let role = ['Attack', 'Aerobatic', 'Aerial crane', 'Aerial refueling', 'AWACS', 'Bomber', 'Cargo', 'Combat', 'Fighter',
+'Ground attack', 'Missile', 'Maritime patrol', 'Multi role', 'Reconnaissance',
+'Surveillance', 'Trainer', 'Transport', 'Utility' ];
+
+let industry = ['commercial', 'private', 'military', 'aerospace'];
+
+let type = ['Agriculture', 'Amphibian', 'Bomber', 'Carrier', 'Commercial',
+'Commuter', 'Drone', 'Experimental', 'Fighter jet', 'Float', 'Jet',
+'Light sports aircraft', 'Multi engine props', 'Piston helicopters',
+'Single engine props', 'Turbine helicopters', 'Turboprops',
+'Very light jet', 'VTOL', 'UAV'];
+
+let category = ['Airplane', 'Airship', 'Balloon', 'Drone', 'Glider', 'Helicopter', 'Space shuttle', 'UAV'];
+
+let engineType = ['Electric motor', 'Piston engine', 'Eingle engine', 'Turboprop'];
+
+let productionStatus = ['In production', 'In development', 'Out of production'];
+
+let airCraftManufacturers = ['AAR CORP', 'Airbus', 'AgustaWestland',
+'AviaBellanca', 'Atlas Aviation', 'BAE Systems', 'Bell Helicopter', 'Beechcraft', 'Boeing', 'Bombardier Aerospace',
+'Cessna Aircraft Company', 'Cirrus Design', 'Dassault Aviation', 'Diamond Aircraft Industries',
+'Embraer', 'Eurocopter', 'General Dynamics Corporation', 'Gulfstream Aerospace', 'Honda Jet', 'Hindustan Aeronautics Limited',
+'Lockheed Martin', 'Mooney Aircraft', 'Mil', 'Northrop Grumman', 'Piaggio Aero Industries',
+'Pilatus Aircraft', 'Piper Aircraft', 'RSK MiG', 'Kamov', 'Raytheon Company', 'Robinson Helicopter Company', 'Rolls-Royce',
+ 'Safran Group', 'Sukhoi', 'SAAB', 'Sikorsky', 'Textron Aviation', 'Tupolev', 'United Technologies Corporation'];
+
+let engineManufacturers = ['General Electric', 'Pratt & Whitney', 'Rolls-Royce',
+'Safran Group', 'United Technologies Corporation'];
+
 
 module.exports = {
   role: role,
-  industry: industry
+  industry: industry,
+  type: type,
+  category: category,
+  engineType: engineType,
+  airCraftManufacturers: airCraftManufacturers,
+  engineManufacturers: engineManufacturers,
+  productionStatus: productionStatus
 };
--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,7 @@
 module.exports = function(app){
 	var main = require('./controllers/main.controller.js');
 	var user = require('./controllers/user.controller.js');
+	var editor = require('./controllers/admin.editor.controller');
 
 	// 'CORE' ----------------------------
 	app.route('/').get(main.index);
@@ -11,6 +12,14 @@ module.exports = function(app){
 	app.route('/login').post(user.login);
 	app.route('/signup').post(user.signup);
 	app.route('/api/profile').get(user.profile);
+
+	// 'ADMIN'
+  app.route('/api/admin/editor').post(editor.createCamp);
+  app.route('/api/admin/editor').get(editor.retriveCamps);
+  app.route('/api/admin/editor/:airCraftId').get(editor.retriveCamp);
+  app.route('/api/admin/editor/:airCraftId').put(editor.updateCamp);
+  app.route('/api/admin/editor/:airCraftId').delete(editor.deletCamp);
+  app.param('airCraftId', editor.airCraftId);
 
 
 };
